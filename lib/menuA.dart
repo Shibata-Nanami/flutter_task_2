4a337f137e5b48d24d927c121b9d72cebb08541d
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-10 16:39:44 +0900

    メニューA画面のボタン作成

--- lib/menuA.dart
+++ lib/menuA.dart
@@ -7,13 +7,20 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('About')),
+      appBar: AppBar(title: const Text('メニューA')),
       body: Center(
         child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            fixedSize: const Size(280, 50),
+            backgroundColor: Colors.blueAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
           onPressed: () {
             context.go('/');
           },
-          child: const Text('Back to Home'),
+          child: const Text('メニューA'),
         ),
       ),
     );
